cb8dbdd26c8fc43414f3d9137b107623ce3cc303
Dev7896 <barmandev402@gmail.com>
2025-01-17 22:21:57 +0530

    tailwind css setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust the paths based on your project structure
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
